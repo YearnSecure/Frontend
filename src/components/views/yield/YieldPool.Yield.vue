61664b25a549c77db86cdf4efe784062cd7a6cef
YearnSecure <71983856+YearnSecure@users.noreply.github.com>
2021-04-10 11:23:30 +0400

    fix button bug

--- src/components/views/yield/YieldPool.Yield.vue
+++ src/components/views/yield/YieldPool.Yield.vue
@@ -98,12 +98,29 @@
           <div class="flex">
             <button
                 v-if="yieldPool.rewards && yieldPool.rewards > 0"
-                v-on:click="claim" class="flex-1 text-center py-2 px-4 m-auto rounded bg-yellow-500 hover:bg-yellow-600">
+                v-on:click="claim" class="
+                flex-1 text-center py-2 px-4
+                m-auto rounded
+                bg-yellow-500
+                hover:bg-yellow-600">
               <span class="text-white">Claim</span>
             </button>
             <button v-if="yieldPool.accountYsecStaked && yieldPool.accountYsecStaked > 0"
-                    v-on:click="unStake" class="flex-1 ml-3 py-2 px-4 m-auto rounded bg-yellow-500 dark:bg-transparent border border-white hover:bg-yellow-500 hover:border-yellow-500 hover:text-yellow-500">
-              <span class="text-white">Unstake</span>
+                    v-on:click="unStake" class="
+                    flex-1 ml-3 py-2 px-4 m-auto rounded
+                    bg-transparent
+                    border
+                    border-yellow-500
+                    dark:border-white
+                    text-yellow-500
+                    text-white
+                    hover:bg-yellow-500
+                    hover:text-white
+                    dark:text-white
+                    dark:bg-transparent
+                    dark:hover:border-yellow-500
+                    dark:hover:text-yellow-500">
+              <span>Unstake</span>
             </button>
           </div>
         </div>
